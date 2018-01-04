7042acb359c1c83734c1eb8c847c82d7fc8859d2
Hans Christian Reinl <info@drublic.de>
2018-01-04 11:54:41 +0100

    Remove datetime dependency, not used

--- Form/FormFieldDate.jsx
+++ Form/FormFieldDate.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import dateFormat from 'dateformat'
 
 import KeyboardArrowLeftIcon from 'material-ui-icons/KeyboardArrowLeft'
 import KeyboardArrowRightIcon from 'material-ui-icons/KeyboardArrowRight'
