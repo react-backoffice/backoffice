cb28ca9ef89180b6cd6cd7bb8b38531eb49be898
Hans Christian Reinl <info@drublic.de>
2018-02-19 11:31:59 +0100

    Implement production version with prepublish script
    Closes #2

---
+++ webpack-production.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const LIBRARY_NAME = 'backoffice'
+
+module.exports = {
+  entry: {
+    backoffice: path.resolve(__dirname, 'index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    filename: `${LIBRARY_NAME}.js`,
+    library: LIBRARY_NAME,
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+  },
+  externals: [
+    'react',
+    'react-dom',
+    'react-router-dom',
+    'prop-types',
+    /^(material-ui\/.*)$/i,
+  ],
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader?babelrc=.babelrc.production',
+      },
+    }],
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx',
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+  ],
+}
