47cc64df596dd7b9dec089c8d7fb25e9353b3ec2
Hans Christian Reinl <info@drublic.de>
2018-02-27 17:13:00 +0100

    Listing Search: Add more tests

--- src/Listing/ListingSearch.test.jsx
+++ src/Listing/ListingSearch.test.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
+import Enzyme, { mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
+import IconButton from 'material-ui/IconButton/IconButton'
 import ListingSearch from './ListingSearch'
+
+Enzyme.configure({ adapter: new Adapter() })
 
 it('renders correctly', () => {
   const tree = renderer
@@ -26,4 +31,55 @@ it('renders correctly when open', () => {
     ))
     .toJSON()
   expect(tree).toMatchSnapshot()
+})
+
+it('opens if click is triggered', () => {
+  const listingSearch = mount((
+    <ListingSearch
+      open
+      onClick={() => { }}
+      onFilter={() => { }}
+    />
+  ))
+
+  expect(listingSearch.instance().state.open).toEqual(false)
+
+  listingSearch.find(IconButton).simulate('click')
+
+  expect(listingSearch.instance().state.open).toEqual(true)
+})
+
+it('filters if open and value changes', () => {
+  const onFilter = jest.fn()
+  const listingSearch = mount((
+    <ListingSearch
+      open
+      onClick={() => { }}
+      onFilter={onFilter}
+    />
+  ))
+
+  listingSearch.find('input').simulate('change', { value: 'a' })
+
+  expect(onFilter).toHaveBeenCalled()
+})
+
+it('does not filter if not open and value changes', () => {
+  const onFilter = jest.fn()
+  const listingSearch = mount((
+    <ListingSearch
+      open
+      onClick={() => { }}
+      onFilter={onFilter}
+    />
+  ))
+
+  expect(listingSearch.instance().state.open).toEqual(false)
+  listingSearch.find(IconButton).simulate('click')
+
+  expect(listingSearch.instance().state.open).toEqual(true)
+
+  listingSearch.find('input').simulate('change', { value: 'a' })
+
+  expect(onFilter).toHaveBeenCalled()
 })
