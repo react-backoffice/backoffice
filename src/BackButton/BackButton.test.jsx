4027f9b4167405c2c94d3b195299ecc86ebe8ff6
Hans Christian Reinl <info@drublic.de>
2018-03-02 17:00:40 +0100

    Backbutton: Increase test coverage

--- src/BackButton/BackButton.test.jsx
+++ src/BackButton/BackButton.test.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import MockRouter from 'react-mock-router'
 import renderer from 'react-test-renderer'
+import Enzyme, { mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
 import BackButton from './'
 
-it('renders correctly', () => {
-  const tree = renderer
-    .create((<Router><BackButton url="/" /></Router>))
-    .toJSON()
-  expect(tree).toMatchSnapshot()
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('Back Button', () => {
+  it('renders correctly', () => {
+    const tree = renderer
+      .create((<MockRouter><BackButton url="/" /></MockRouter>))
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('updates history on click', () => {
+    const push = jest.fn()
+    const button = mount(<MockRouter push={push}><BackButton url="/" /></MockRouter>)
+
+    button.find(BackButton).simulate('click')
+
+    expect(push).toHaveBeenLastCalledWith('/')
+  })
 })
