e6e80ce33bf15070ab7b5f88e0b781075363bb23
Hans Christian Reinl <info@drublic.de>
2018-04-20 13:46:26 +0200

    Base: Add more tests

--- src/Base/Base.test.jsx
+++ src/Base/Base.test.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import MockRouter from 'react-mock-router'
 import Enzyme, { mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import { Typography, IconButton, Button } from 'material-ui'
 
 import Base from './'
 
 import menuData from '../../__tests__/data/menu'
+import Header from '../Header'
+import Drawer from '../Drawer'
+import CookieInfo from '../CookieInfo'
 
 Enzyme.configure({ adapter: new Adapter() })
 
@@ -49,7 +53,7 @@ describe('Base', () => {
     const tree = mount((
       <MockRouter>
         <Base
-          open
+          isOpen
           title="Foo"
           menuData={menuData}
           hasCookieInfo
@@ -60,6 +64,87 @@ describe('Base', () => {
         </Base>
       </MockRouter>
     ))
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('click on title', () => {
+    const tree = mount((
+      <MockRouter>
+        <Base
+          isOpen
+          title="Foo"
+          menuData={menuData}
+          hasCookieInfo
+          menuOpen
+        >
+          <div>Foo</div>
+        </Base>
+      </MockRouter>
+    ))
+
+    tree.find(Header).find(Typography).simulate('click')
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('click on menu icon if open changes state', () => {
+    const tree = mount((
+      <MockRouter>
+        <Base
+          isOpen
+          title="Foo"
+          menuData={menuData}
+          hasCookieInfo
+          menuOpen
+        >
+          <div>Foo</div>
+        </Base>
+      </MockRouter>
+    ))
+
+    tree.find(Header).find(IconButton).simulate('click')
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('click on menu icon if closed changes state', () => {
+    const tree = mount((
+      <MockRouter>
+        <Base
+          isOpen={false}
+          title="Foo"
+          menuData={menuData}
+          hasCookieInfo
+          menuOpen
+        >
+          <div>Foo</div>
+        </Base>
+      </MockRouter>
+    ))
+
+    tree.find(Drawer).find(IconButton).simulate('click')
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('call function when cookie is accepted', () => {
+    const tree = mount((
+      <MockRouter>
+        <Base
+          isOpen={false}
+          title="Foo"
+          menuData={menuData}
+          hasCookieInfo
+          menuOpen
+        >
+          <CookieInfo />
+          <div>Foo</div>
+        </Base>
+      </MockRouter>
+    ))
+
+    tree.find(CookieInfo).find(Button).simulate('click')
 
     expect(tree).toMatchSnapshot()
   })
