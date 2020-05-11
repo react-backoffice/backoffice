722c9e3fb4a67cd03ace6b8fc9248f8c50443e87
Hans Christian Reinl <info@drublic.de>
2020-05-11 16:11:22 +0200

    Prittier: fix index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Container from "./tests/Container";
+
+const element = document.querySelector("[data-react-app]");
+
+const render = (Component) => {
+  ReactDOM.render(<Component />, element);
+};
+
+render(Container);
